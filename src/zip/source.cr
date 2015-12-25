4382434453123ad7da0d0ec1b3b992d6bc38fa84
Paul Duncan <pabs@pablotron.org>
2015-12-25 06:15:21 -0500

    add minimal documentation

--- src/zip/source.cr
+++ src/zip/source.cr
@@ -1,5 +1,6 @@
 
 module Zip
+  # Abstract base source for a `Archive#add` or `Archive#replace`.
   class Source
     protected getter source
 
@@ -10,18 +11,21 @@ module Zip
     end
   end
 
+  # Use string as a source for a `Archive#add` or `Archive#replace`.
   class StringSource < Source
     def initialize(zip : Archive, s : String)
       super(zip, LibZip.zip_source_buffer(zip.zip, s, s.bytesize, 0))
     end
   end
 
+  # Use slice as a source for a `Archive#add` or `Archive#replace`.
   class SliceSource < Source
     def initialize(zip : Archive, s : Slice)
       super(zip, LibZip.zip_source_buffer(zip.zip, s, s.bytesize, 0))
     end
   end
 
+  # Use file as a source for a `Archive#add` or `Archive#replace`.
   class FileSource < Source
     def initialize(
       zip         : Archive,
