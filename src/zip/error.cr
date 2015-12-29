9b5b99cadeead13fb98d75fd0bcfba8b720b3823
Paul Duncan <pabs@pablotron.org>
2015-12-29 15:41:59 -0500

    document Error

--- src/zip/error.cr
+++ src/zip/error.cr
@@ -1,11 +1,16 @@
 module Zip
+  # Wrapper for exceptions raised by most `Archive` and `File` methods.
   class Error < Exception
-    def initialize(@code : ErrorCode)
+    # Internal wrapper to create a new `Error` by wrapping an
+    # `ErrorCode` value.
+    protected def initialize(@code : ErrorCode)
       # puts "error code = #{@code}"
       super(@code.message)
     end
 
-    def initialize(num : Int32)
+    # Internal wrapper to create a new `Error` from arbitrary integer
+    # value *num*.
+    protected def initialize(num : Int32)
       @code = ErrorCode.new(num)
       super(@code.message)
     end
